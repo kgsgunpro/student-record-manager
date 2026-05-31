89d1f9bbd5f4b8511b9fce573b9160a7f2076e9e
kgsgun <kgsgunpro@gmail.com>
2026-05-31 18:45:08 +0530

    dev-v2 with add menu , student creation and search by id

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
-class Student:
-    def __init__(self,id,name,grade):
-        self.id=id
-        self.name=name
-        self.grade=grade
-    def print_details(self):
-        print(f'id: {self.id} , name: {self.name} , grade :{self.grade} ')
-
 class Student_manager:
     def __init__(self):
         self.next_id = 1
-        self.student_list =[]
         self.student_dict={}
     def add_student(self,name,grade):
-        s = Student(self.next_id ,name,grade)
+        
         self.student_dict[self.next_id] = {'name' : name , 'grade':grade}
         self.next_id+=1
-        self.student_list.append(s)
         
     def show_student(self,student_id):
         name=self.student_dict[student_id]['name']
@@ -26,15 +16,26 @@ class Student_manager:
 sm=Student_manager()
 def menu():
     while True:
-        inp = int(input('Main menu \n' \
-        '1.press 1 to add student :'))
-        if inp==1:
-            name=input('Enter student name : ')
-            grade=input('Enter grade : ')
-            sm.add_student(name,grade)
+        try:
+            inp = int(input('Main menu \n' \
+            '1.press 1 to add student \n'
+            '2.press 2 to fetch details of student'
+            'press any number to exit'))
+            if inp==1:
+                name=input('Enter student name : ')
+                grade=input('Enter grade : ')
+                sm.add_student(name,grade)
 
-            print(f'the follwing details added to database \n {sm.show_student(sm.next_id -1)}')
-        else:
-            break
+                print(f'the follwing details added to database \n {sm.show_student(sm.next_id -1)}')
+            elif inp==2:
+                try :
+                    student_id = int(input('Enter student id'))
+                    print(sm.show_student(student_id))
+                except Exception as e:
+                    print('id doesn\'t exist ', e ,type(e))
+            else:
+                break
+        except:
+            print('please enter integer')
 menu()
             
